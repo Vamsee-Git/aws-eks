8515b0c617c3917e4164d0b78141b06e9e41229b
Vamsee-Git <95101547+Vamsee-Git@users.noreply.github.com>
2025-04-08 06:55:39 +0530

    Update backend.tf

--- Infra/backend.tf
+++ Infra/backend.tf
@@ -1,7 +1,7 @@
 terraform{
   backend "s3" {
     bucket         = "my-terraform-state-bucket-two-tier-vamsee"
-    key            = "terraform/eks_cluster_statefile1"
+    key            = "terraform/eks_cluster_statefile"
     region         = "ap-south-1"
     encrypt        = true
   }
